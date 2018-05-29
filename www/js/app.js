a463b92c4375a08b1ba4b9c72cde557fcb794263
Gsús Gómm <g3xv5@live.com.mx>
2018-05-28 19:24:21 -0500

    update 2
    Actualización de Leyenda de Versión

--- www/js/app.js
+++ www/js/app.js
@@ -462,7 +462,6 @@ function cargarSesion(){
     var len = results.rows.length;
     if(len == 0){ console.log('No existe usuario en sesión.'); $("#usr_pic").html("<label>Cargando Usuario...</label>"); cargarSesion();
     } else{
-      $("#lblBuild").text(" Versión 1.0.47 (Build 240518)");
       $("#lblBuild").text(" Versión 1.0.47 (Build 280518)");
       $("#loginscreen").hide();
       /*Validamos usuario Jesus Gómez/Rafael Iñiguez*/
